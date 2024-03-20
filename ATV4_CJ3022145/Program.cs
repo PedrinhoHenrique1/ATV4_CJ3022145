2ab084eee6e9406c515e9b21dd5c70823c0913c0
CJ3022145 <CJ3022145@LAB01-PC26>
2024-03-20 08:25:23 -0300

    Adicionar arquivos de projeto.

---
+++ ATV4_CJ3022145/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Escolha uma das seguintes opções: \n 1 - Débito \n 2 - Crédito \n 3 - Pix \n 4 - Boleto \n 5 - Transferência bancária");
+int opc = int.Parse(Console.ReadLine());
+
+switch (opc)
+{
+    case 1:
+        Console.WriteLine("O pagamento será feito em débito");
+        break;
+    case 2:
+        Console.WriteLine("O pagamento será feito no crédito");
+        Console.WriteLine("Haverá um acréscimo de 5%");
+        break;
+    case 3:
+        Console.WriteLine("O pagamento será feito no pix");
+        Console.WriteLine("Haverá um desconto de 5%");
+        break;
+    case 4:
+        Console.WriteLine("O pagamento será feito no boleto");
+        Console.WriteLine("Haverá um acréscimo de 5%");
+        break;
+    case 5:
+        Console.WriteLine("O pagamento será feito com transferência bancária");
+        break;
+}
